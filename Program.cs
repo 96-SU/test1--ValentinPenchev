97b2e798ad658af5717c0a9786974b8666ee49e4
ValentinPenchev <125907194+ValentinPenchev@users.noreply.github.com>
2026-03-20 08:37:31 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+
+        int[] count = new int[10];
+
+        for (int i = a; i <= b; i++)
+        {
+            int num = i;
+
+            while (num > 0)
+            {
+                int digit = num % 10;
+                count[digit]++;
+                num /= 10;
+            }
+        }
+
+        int maxCount = 0;
+        int resultDigit = 0;
+
+        for (int d = 0; d <= 9; d++)
+        {
+            if (count[d] > maxCount)
+            {
+                maxCount = count[d];
+                resultDigit = d;
+            }
+        }
+
+        Console.WriteLine($"Digit {resultDigit} - {maxCount} times");
+    }
+}
